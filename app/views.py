f28a58d739a2a494bb8b66ca59dfa465c709d3bb
BwoyBarnes <odanebarnes@outlook.com>
2018-05-22 00:58:54 -0500

    Updated home page

--- app/views.py
+++ app/views.py
@@ -60,7 +60,7 @@ def terms():
 @app.route('/restaurants/')
 def restaurants():
     """Render the website's restaurants page."""
-    return render_template('home.html')
+    return render_template('restaurants.html')
 
 @app.route('/restaurants/<restaurantName>')
 def viewRestaurant(restaurantName):
